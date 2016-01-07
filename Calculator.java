6efb9fed7495426b04e69fc606488859e25d8085
mmcs2012 <mmcs@yandex.ru>
2016-01-08 02:57:59 +0300

    add operation minus

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,11 @@ public class Calculator {
 			this.result += param;
 		}
 	}
+	public void minus(int ... params) {
+		for (Integer param : params) {
+			this.result = params[0] - params[1];
+		}
+	}
 	public int getResult() {
 		return this.result;
 	}
--- InteractRunner.java
+++ InteractRunner.java
@@ -11,7 +11,23 @@ public class InteractRunner {
 				String first = reader.next();
 				System.out.println("Enter second arg: ");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
+				System.out.println("Enter operation type: add/minus");
+				String operation_type = reader.next();
+				switch (operation_type.toLowerCase()) {
+					case "add":
+						System.out.println("You decide add");
+						calc.add(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					case "minus":
+						System.out.println("You decide minus");
+						calc.minus(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					default:
+						System.out.println("Unknown operation, default operation");
+						calc.add(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+				}
+				
 				System.out.println("Result: " + calc.getResult());
 				calc.cleanResult();
 				System.out.println("Exit: yes/no ?");
